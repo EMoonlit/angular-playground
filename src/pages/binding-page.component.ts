ddff6a91d8e6c8d97fca0f8885b48eeaeac984ef
Emerson Junqueira <emoonlit@gmail.com>
2023-10-08 20:48:28 -0300

    feat: added bind in class style

--- src/pages/binding-page.component.ts
+++ src/pages/binding-page.component.ts
@@ -5,10 +5,31 @@ import { Component } from "@angular/core";
     template: `
         <h2>Angular image</h2>
         <img [src]="image" [style]="{width: width}" />
-    `
+        <div class="alert" [class]="{isSuccess:isSuccess}">
+            Alert
+        </div>
+
+        <button (click)="toggle()">Toggle Background</button>
+    `,
+    styles: [
+        `.alert {
+            width: 200px;
+            height: 100px;
+            border: 1px solid green;
+        }
+        .isSuccess {
+            background-color: green;
+        }`
+    ]
 })
 
 export class BindingPageComponent {
     width = "200px"
     image = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/800px-Angular_full_color_logo.svg.png"
+
+    isSuccess = true
+
+    toggle() {
+        this.isSuccess = !this.isSuccess
+    }
 }
