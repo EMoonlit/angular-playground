6cfe79769fa371922127b278fbe941fae4d05296
Emerson Junqueira <emoonlit@gmail.com>
2023-10-12 16:29:56 -0300

    feat: added pipe operators map tap and filter

--- src/pages/rxjs-page.component.ts
+++ src/pages/rxjs-page.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from "@angular/core";
-import {interval, of, Subscription} from "rxjs";
+import {filter, interval, map, of, range, Subscription, tap} from "rxjs";
 
 @Component({
   selector: 'app-rxjs',
@@ -16,13 +16,26 @@ export class RxJsPageComponent implements OnInit, OnDestroy {
 
   subscription! : Subscription
 
+  observableExThree = range(1, 10)
+
   constructor() {
   }
 
   ngOnInit() {
     this.observableExOne.subscribe(i => this.items.push(i))
 
-    this.subscription = this.observableExTwo.subscribe(n => this.numbers.push(n))
+    // this.subscription = this.observableExTwo.subscribe(n => this.numbers.push(n))
+    this.subscription = this.observableExTwo
+      .pipe(
+        map(x => x * 3),
+        tap(x => console.log(x)),
+        filter(x => x % 2  === 0)
+      )
+      .subscribe(n => this.numbers.push(n))
+
+    this.observableExThree.subscribe(
+      i => this.numbers.push(i)
+    )
   }
 
   unsubscribe() :void {
